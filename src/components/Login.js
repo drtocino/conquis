c702fa9dba7a09e6d106a19a22dcfe713285b171
dilan504 <dch19940@gmail.com>
2023-04-09 16:29:23 -0400

    Login with Google Oauth started, understanding a wrapping provider

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,13 +1,22 @@
 import React from 'react';
 // import GoogleLogin from 'react-google-login';
+import { GoogleLogin, GoogleOAuthProvider } from '@react-oauth/google';
 
 const Login = () => {
 
     // const REACT_GOOGLE_AUTH = "965906568095-71mcrc2ge14hk8pp7ln65v4n5kb7cpdf.apps.googleusercontent.com";
     return (
     <div>
-        Login
-        {/* <GoogleLogin clientId={REACT_GOOGLE_AUTH} buttonText="Ingresa con Google" onSuccess={console.log("Exito")} onFailure={console.log("Error")} cookiePolicy={"single_host_origin"} /> */}
+        <GoogleOAuthProvider clientId="965906568095-71mcrc2ge14hk8pp7ln65v4n5kb7cpdf.apps.googleusercontent.com">
+                <GoogleLogin
+          onSuccess={credentialResponse => {
+            console.log(credentialResponse);
+          }}
+          onError={() => {
+            console.log('Login Failed');
+          }}
+        />
+        </GoogleOAuthProvider>
     </div>
     )
 }
